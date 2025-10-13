f9ad86ee5a90d2f891db8eeb6364c0bf92a05e28
Gabriel Mendes <gabriel.mendes17@outlook.com>
2025-10-13 11:21:49 -0300

    feature: add spring security

---
+++ src/main/java/med/voll/api/dto/usuario/Login.java
@@ -0,0 +1,12 @@
+package med.voll.api.dto.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record Login(
+    @NotBlank(message = "Login é obrigatório")
+    String login,
+    @NotBlank(message = "Senha é obrigatória")
+    String senha
+) {
+    
+}
